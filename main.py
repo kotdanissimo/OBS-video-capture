eed4d1c63abaaf4325f42340c11fb15ccf58f974
kotdanissimo <142153274+kotdanissimo@users.noreply.github.com>
2024-01-18 15:43:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+
+
+def main():
+    # Open a video stream from OBS (change the index if needed)
+    cap = cv2.VideoCapture(1)
+
+    # Check if the video stream is successfully opened
+    if not cap.isOpened():
+        print("Error: Unable to open video stream from OBS.")
+        exit()
+
+    # Set the frame height and width
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+
+    while True:
+        # Read a frame from the video stream
+        ret, frame = cap.read()
+        print(frame.shape)
+
+        # Check if the frame is successfully read
+        if not ret:
+            print("Error: Unable to read frame.")
+            break
+
+        # Display the frame
+        cv2.imshow("OBS Stream", frame)
+
+        # Break the loop if 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video stream and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # Call the main function if the script is executed
+    main()
