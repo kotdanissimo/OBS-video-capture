679d000eb59ff73e3a74da38a3f4395c65d88525
kotdanissimo <142153274+kotdanissimo@users.noreply.github.com>
2024-06-24 17:26:42 +0300

    transform to class

--- main.py
+++ main.py
@@ -1,58 +1,114 @@
-# This code and the opencv library are licensed under the Apache License, version 2.0.
-# More details can be found in the LICENSE file or at http://www.apache.org/licenses/
 import cv2
 
 
-def main(height=720, width=1280, stop_keys=None):
+class VideoCapture:
     """
-    Main function to capture video from OBS and display it.
+    A class used to capture video from a camera or a video file.
 
-        Create a VideoCapture object
-        Argument can be either the device index or the name of a video file.
-        Device index is just the number to specify which camera.
-        Normally one camera will be connected, so we pass 0.
-        You can select the second camera by passing 1 and so on.
+    ...
+
+    Attributes
+    ----------
+    height : int
+        the height of the video frame
+    width : int
+        the width of the video frame
+    stop_keys : list
+        the keys that will stop the video capture
+
+    Methods
+    -------
+    capture():
+        Captures video frames and yields them one by one.
+    show_stream():
+        Displays the video stream in a window.
     """
 
-    if stop_keys is None:
-        stop_keys = ['q', 'Q']
+    def __init__(self, height=720, width=1280, stop_keys=None):
+        """
+        Constructs all the necessary attributes for the VideoCapture object.
 
-    cap = cv2.VideoCapture(1)
+        Parameters
+        ----------
+            height : int
+                the height of the video frame
+            width : int
+                the width of the video frame
+            stop_keys : list
+                the keys that will stop the video capture
+        """
 
-    # Check if camera opened successfully
-    if not cap.isOpened():
-        print("Error: Unable to open video stream from OBS.")
-        exit()
+        self.height = height
+        self.width = width
 
-    # Set video frame height and width
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        if stop_keys is None:
+            self.stop_keys = ['q', 'Q']
+        else:
+            self.stop_keys = stop_keys
 
-    # Infinite loop until video capture is stopped
-    while True:
-        # ret is a boolean indicating if it was successful.
-        # frame is the current frame being read.
+    def capture(self):
+        """
+        Captures video frames and yields them one by one.
+
+        Yields
+        ------
+        numpy.ndarray
+            a frame captured from the video
+        """
+
+        # Create a VideoCapture object
         try:
-            ret, frame = cap.read()
-            print(frame.shape)
+            cap = cv2.VideoCapture(1)
         except Exception as e:
             print("Error" + str(e))
-            break
+            return
 
-        # Display the resulting frame
-        cv2.imshow("OBS Stream", frame)
+        # Set video frame height and width
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
 
-        # Wait for user to press 'q' key to stop the program
-        key = chr(cv2.waitKey(1) & 0xFF)
-        if key in stop_keys:
-            break
+        # Infinite loop until video capture is stopped
+        while True:
+            # ret is a boolean indicating if it was successful.
+            # frame is the current frame being read.
+            try:
+                ret, frame = cap.read()
+                # print(frame.shape)
 
-    # After the loop release the cap object
-    cap.release()
-    # Destroy all the windows
-    cv2.destroyAllWindows()
+                yield frame
+
+            except Exception as e:
+                print("Error" + str(e))
+                continue
+
+        # After the loop release the cap object
+        cap.release()
+        # Destroy all the windows
+        cv2.destroyAllWindows()
+
+    def show_stream(self):
+        """
+        Displays the video stream in a window.
+
+        The video stream is displayed in a window until one of the stop keys is pressed.
+        """
+
+        for frame in self.capture():
+            # Display the resulting image
+            cv2.imshow("OBS Stream", frame)
+
+            # Wait for user to press stop keys to stop the program
+            key = chr(cv2.waitKey(1) & 0xFF)
+            if key in self.stop_keys:
+                break
 
 
 if __name__ == "__main__":
-    # Execute main function
-    main(stop_keys=[' '])
+    # Create a VideoCapture object and start displaying the video stream
+    # VideoCapture(stop_keys=[' ']).show_stream()
+
+    # Create a VideoCapture object and start capturing video frames
+    video_capture = VideoCapture(stop_keys=[' '])
+    for img in video_capture.capture():
+        # Print the shape of each captured frame
+        print(img)
